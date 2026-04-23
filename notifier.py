896554227fff065037db93d3efc1cbcf2a2aa34b
Smart-Rwl <110030446+Smart-rwl@users.noreply.github.com>
2026-04-23 17:54:36 +0530

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,6 +1,8 @@
 import requests
 from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
+MESSAGE_THREAD_ID = 1373
+
 def send(msg):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     requests.post(url, data={"chat_id": TELEGRAM_CHAT_ID, "text": msg})
