fed3d5e3afd3b01f1e00cfac3cc80527419623bb
Smart-Rwl <110030446+Smart-rwl@users.noreply.github.com>
2026-04-23 13:13:30 +0530

    Add function to initialize Google Sheets

---
+++ sheets.py
@@ -0,0 +1,12 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from config import GOOGLE_CREDS, SHEET_NAME
+
+def init_sheet():
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive"
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(GOOGLE_CREDS, scope)
+    client = gspread.authorize(creds)
+    return client.open(SHEET_NAME).sheet1
