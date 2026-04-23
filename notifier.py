166b09e6e07a7b8581c49e1ebc76563c47855bb7
Smart-Rwl <110030446+Smart-rwl@users.noreply.github.com>
2026-04-23 17:58:27 +0530

    Include message_thread_id in send function
    Add message_thread_id to Telegram message payload

--- notifier.py
+++ notifier.py
@@ -1,8 +1,15 @@
 import requests
 from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
-MESSAGE_THREAD_ID = 1373
+MESSAGE_THREAD_ID = 1373  # your topic ID
 
 def send(msg):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": TELEGRAM_CHAT_ID, "text": msg})
+    
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": msg,
+        "message_thread_id": MESSAGE_THREAD_ID
+    }
+
+    requests.post(url, data=payload)
